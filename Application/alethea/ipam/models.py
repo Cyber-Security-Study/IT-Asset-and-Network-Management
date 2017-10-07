0258aae2f23148a6910c09ea90209a9bbdce0e87
Cameron Gray <cameron@camerongray.me>
2017-10-07 23:57:48 +0100

    Initial commit of Django app with some models for IPAM in place

---
+++ Application/alethea/ipam/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Subnet(models.Model):
+    address = models.CharField(max_length=255)
+    netmask = models.IntegerField()
+    name = models.CharField(max_length=255)
+
+
+class DhcpRange(models.Model):
+    start_address = models.CharField(max_length=255)
+    end_address = models.CharField(max_length=255)
+    subnet = models.ForeignKey(Subnet, on_delete=models.CASCADE)
+
+
+class IpAddress(models.Model):
+    address = models.CharField(max_length=255)
+    descripton = models.TextField()
+    is_failover = models.BooleanField()
+    subnet = models.ForeignKey(Subnet, on_delete=models.CASCADE)
+
+# TODO: Implement Address Assignments Table!
